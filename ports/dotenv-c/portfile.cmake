ab30d2374ea617e2268a8da64ec4d950dd99e1a6
Ligo George <lijopppans@gmail.com>
2023-11-04 21:58:15 +0100

    updating dotenv

--- ports/dotenv-c/portfile.cmake
+++ ports/dotenv-c/portfile.cmake
@@ -1,17 +1,17 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO Isty001/dotenv-c
-  REF 494e8062e2dae17169c9c67998bb7aba27cdaaf4
-  SHA512 184556f3add122e087326ba82556355dbdc08ed5b365f6ebef7e7bd6263f536033b2ef87f25d29032ddc51abb5967290565ee29b929de14c7f76842c83905f03
+  REPO lijoppans/dotenv-c
+  REF 2905632f6ee8853a778896a3afd4ea9f8ce01443
+  SHA512 7aef6ff4964d08f18e02c575ebe64bec375da7184559399837a4238af8c8227a69b4363cd2ad27b51e5489e6fcbb64c3e52ee4576b54d27ba41d2a7b367c7c8a
   HEAD_REF master
 )
 
 vcpkg_configure_cmake(
   SOURCE_PATH "${SOURCE_PATH}"
-  PREFER_NINJA
 )
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets()
+vcpkg_fixup_cmake_targets(
+)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
